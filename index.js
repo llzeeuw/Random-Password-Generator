dafce24c0f68f31913be8e8d8cb4f322e5a999b7
llzeeuw <llzeeuw@users.noreply.github.com>
2024-02-23 16:12:24 +0100

    Add generate password functionality
    Add generate password functionality.
    Caused bug that passwords are generated upon loading the app. Needs fixing

--- index.js
+++ index.js
@@ -5,12 +5,36 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+/* Declare variable in which password length can be stored. Standard length is set at 12.*/
+let passwordLength = 12;
 
+/* Retrieve password elements from HTML file and store them in a JavaScript variable*/
+let passwordEL1 = document.getElementById("password-el1");
+let passwordEL2 = document.getElementById("password-el2");
 
 /* Create on-click functionality button */ 
 
-/* Generate password function */ 
 
 /* Set password length according to user input */ 
+
+
+
+/* Generate password function */ 
+function generatePasswords(){
+    //Declare password and assign an empty string to it
+    let password = "";
+    // Set password length according to user input
+    for (let i = 0; i < passwordLength; i++){
+    //Generate random password by looping over characters array
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+    }
+    return password;
+}
+
+/* Store passwords in PasswordEL's and show them in the HTML */ 
+
+passwordEL1.innerHTML = generatePasswords();
+passwordEL2.innerHTML = generatePasswords();
 
 /* Create reset passwords function */
